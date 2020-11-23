2049dcce7fc2f06de73fb5bb2d2fe2e0c6ef7873
FaheemRiaz1 <faheemriaz177@gmail.com>
2020-11-23 06:44:19 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View, StyleSheet,TouchableOpacity,Alert } from 'react-native';
+import { Text, View, StyleSheet,TouchableOpacity } from 'react-native';
 import Constants from 'expo-constants';
 
 
